93cd553ba41cda44557a127bd0a6937d16cda22a
liutao <liutao936@163.com>
2022-06-17 16:57:19 +0800

    Added source code for all chapter_02.

---
+++ chapter_02/favorite_number.py
@@ -0,0 +1,5 @@
+favorite_number = 5
+# 喜欢的数字
+message = f"I favorite number is {favorite_number}."
+# 打印的消息
+print(message)
---
+++ chapter_02/number_eigth.py
@@ -0,0 +1,14 @@
+print(5 + 3)
+print(3 / 2)
+print(3 ** 2)
+print(3 ** 3)
+print(10 ** 6)
+print(2 + 3 * 4)
+print((2 + 3) * 4)
+print(0.1 + 0.1)
+print(4 / 2)
+print(3.0 ** 2)
+universe_age = 14_000_000_000
+print(universe_age)
+x, y, z = 1, 2, 3
+print(x,y,z)
