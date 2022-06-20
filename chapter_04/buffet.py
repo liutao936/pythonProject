30a7b8b31ad93ab2dbd3238e6224b36b0235e144
liutao <liutao936@163.com>
2022-06-20 13:43:17 +0800

    Added source code for all chapter_04.

---
+++ chapter_04/buffet.py
@@ -0,0 +1,9 @@
+foods = ('apple', 'orange', 'banana', 'pear', 'peach')
+for food in foods:
+    print(food)
+
+#foods[4] = 'berry'
+
+foods = ('apple', 'orange', 'banana', 'pear', 'berry')
+for food in foods:
+    print(food)
---
+++ chapter_04/dimensions.py
@@ -0,0 +1,14 @@
+dimensions = (200, 50)
+for dimension in dimensions:
+    print(dimension)
+
+dimensions = (200, 50)
+print("Original dimensions:")
+for dimension in dimensions:
+    print(dimension)
+
+dimensions = (400, 100)
+print("\nModified dimensions:")
+for dimension in dimensions:
+    print(dimension)
+    
