72adbfd187b58896a37f380034d464b2f88cf539
liutao <liutao936@163.com>
2022-06-20 16:03:33 +0800

    Added source code for all chapter_05.

---
+++ chapter_05/hello_admin.py
@@ -0,0 +1,6 @@
+usernames = ['tom', 'jim', 'henry', 'admin', 'jerry']
+for username in usernames:
+    if username == 'admin':
+        print(f"Hello {username.title()}, would you like to see a status report?")
+    else:
+        print(f"Hello {username.title()}, thank you for logging in again.")
---
+++ chapter_05/no_users.py
@@ -0,0 +1,11 @@
+#usernames = ['tom', 'jim', 'henry', 'admin', 'jerry']
+usernames = []
+if usernames:
+    for username in usernames:
+        if username == 'admin':
+            print(f"Hello {username.title()}, would you like to see a status report?")
+        else:
+            print(f"Hello {username.title()}, thank you for logging in again.")
+else:
+    print("We need to find some users!")
+
