f0b6393474205e998e1d9bc933d356f24c31461f
liutao <liutao936@163.com>
2022-06-20 14:43:28 +0800

    Added source code for all chapter_05.

---
+++ chapter_05/more_conditional_tests.py
@@ -0,0 +1,36 @@
+car = 'Subaru'
+
+print("Is car == 'subaru'? I predict True.")
+print(car == 'subaru')
+
+print("Is car == 'subaru'? I predict True.")
+print(car.lower() == 'subaru')
+
+print("\nIs car == 'audi'? I predict False.")
+print(car == 'audi')
+
+age = 20
+
+print("Is age > 18? I predict True.")
+print(age > 18)
+
+print("Is age >= 18? I predict True.")
+print(age >= 18)
+
+print("Is age <= 18? I predict Flase.")
+print(age <= 18)
+
+print("Is age >= 18 and age < 18? I predict True.")
+print(age >= 18 and age <18 )
+
+print("Is age >= 18 or age < 18? I predict True.")
+print(age >= 18 or age <18 )
+
+cars = ['audi', 'bmw', 'subaru', 'toyota']
+
+car = 'bmw'
+if car in cars:
+    print(f"{car.title()} in the list.")
+car = 'wuling'
+if car not in cars:
+    print(f"{car.title()} not in the cars list.")
