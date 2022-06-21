420fd52a80afb723e6d85cb3b879fbdf1f5284ef
liutao <liutao936@163.com>
2022-06-21 16:49:10 +0800

    Added source code for all chapter_06.

---
+++ chapter_06/favorite_numbers.py
@@ -0,0 +1,11 @@
+favorite_numbers = {
+    'joy': '1',
+    'jane': '2',
+    'jim': '3',
+    'tom': '4',
+    'lee': '5'
+    }
+favorite_number = favorite_numbers['joy']
+print(f"Joy's favorite number is {favorite_number}.")
+
+
---
+++ chapter_06/person.py
@@ -0,0 +1,8 @@
+person_info = {
+    'first_name': 'henry',
+    'last_name': 'lee',
+    'age': 18,
+    'city': 'beijing'
+    }
+
+print(person_info)
